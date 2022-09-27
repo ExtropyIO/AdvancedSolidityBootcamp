5c37b47b36a5b815df6b2a155fdaeca901443605
Laurence Kirk <laurence@extropy.io>
2022-09-27 09:23:19 +0100

    Fix exercises

--- exercises/assembly/Assembly_3.sol
+++ exercises/assembly/Assembly_3.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.8.4;
 
 contract SubOverflow {
-    // Modify this function so thaht on overflow it sets the value to 0
+    // Modify this function so that on overflow it returns the value 0
+    // otherwise it should return x - y
     function subtract(uint256 x, uint256 y) public pure returns (uint256) {
         // Write assembly code that handles overflows
         assembly {
