c10d9321c14830e317521407d6c44a35095ec287
Hrvoje <stjepan.hrvoje@gmail.com>
2022-09-30 23:02:47 +0200

    Hardhat optimizer added

--- exercises/gas/hardhat.config.js
+++ exercises/gas/hardhat.config.js
@@ -18,11 +18,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: {
-    compilers: [
-      {
-        version: "0.8.0",
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 100,
       },
-    ],
+    },
   },
   gasReporter: {
     currency: "USD",
