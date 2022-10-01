eb8c9b61c2bb4508b0dea44e916f00b7d08f312a
Hrvoje <stjepan.hrvoje@gmail.com>
2022-10-01 12:28:37 +0200

    Solidity version 0.8.17

--- exercises/gas/hardhat.config.js
+++ exercises/gas/hardhat.config.js
@@ -18,7 +18,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: {
-    version: "0.8.4",
+    version: "0.8.17",
     settings: {
       optimizer: {
         enabled: true,
